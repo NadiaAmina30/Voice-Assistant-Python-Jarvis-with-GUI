62b7eb5b5b6596055f30e3c6d640cf7ccd994391
NadiaAmina30 <66860887+NadiaAmina30@users.noreply.github.com>
2020-07-29 17:15:50 +0530

    Delete textSpeech.py

--- Jarvis/textSpeech.py
+++
@@ -1,6 +0,0 @@
-import pyttsx3 as p
-engine = p.init()
-voices = engine.getProperty("voices")
-engine.setProperty("voice", voices[1].id)
-engine.say("Hi Nadia, what can i do for you?")
-engine.runAndWait()
